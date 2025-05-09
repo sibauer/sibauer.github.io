b00bf2922b7315d82dd2514f88abe90b0410171d
sibauer <sibauer.1@gmx.de>
2025-05-09 17:31:41 +0200

    Flow structure of part 1 (introduction/timeseries)

--- assets/js/plotting.js
+++ assets/js/plotting.js
@@ -1,8 +1,80 @@
 console.log('Started plotting.js:')
 
+// function createPlotly(selector, x_domain, y_domain) {
+//   console.log("created plotly")
+//   const margin = { top: 10, right: 30, bottom: 30, left: 60 },
+//     width = 380 - margin.left - margin.right,
+//     height = 150 - margin.top - margin.bottom;
+
+//   // Create a new Plotly figure
+//   const fig = {
+//     data: [],
+//     layout: {
+//       title: '',
+//       xaxis: {
+//         title: 'Years',
+//         range: x_domain,
+//         type: 'linear'
+//       },
+//       yaxis: {
+//         title: '% of the population infected',
+//         range: y_domain,
+//         type: 'linear'
+//       },
+//       margin: margin,
+//       width: width,
+//       height: height
+//     }
+//   };
+
+//   // Add the plot to the HTML element
+//   Plotly.newPlot(selector, fig);
+
+//   // // Add X axis label
+//   // const xLabel = document.createElement('div');
+//   // xLabel.style.position = 'absolute';
+//   // xLabel.style.top = `${height + margin.top * 2.8}px`;
+//   // xLabel.style.left = `${width / 2.5}px`;
+//   // xLabel.style.fontSize = '12px';
+//   // xLabel.style.fontWeight = 'normal';
+//   // xLabel.textContent = 'Years';
+//   // document.body.appendChild(xLabel);
+
+//   // // Add Y axis label
+//   // const yLabel = document.createElement('div');
+//   // yLabel.style.position = 'absolute';
+//   // yLabel.style.top = `${-height / 0.8}px`;
+//   // yLabel.style.left = `${-margin.left / 1.6}px`;
+//   // yLabel.style.fontSize = '12px';
+//   // yLabel.style.fontWeight = 'normal';
+//   // yLabel.style.transform = 'rotate(-90)';
+//   // yLabel.textContent = '% of the population infected';
+//   // document.body.appendChild(yLabel);
+
+//   return fig;
+// }
+
+// function plotTimeseries(fig, data, color, opacity = 1, transition = 100) {
+//   console.log("log")
+//   // Add the line
+//   const line = {
+//     x: data.map(d => d.t),
+//     y: data.map(d => d.I),
+//     mode: 'lines',
+//     line: {
+//       color: color,
+//       width: 1.5,
+//       opacity: opacity
+//     }
+//   };
+//   fig.data.push(line);
+//   // Plotly.relayout(fig, { xaxis: { range: [d3.min(data, d => d.t), d3.max(data, d => d.t)] } });
+//   return line;
+// }
 // Function to plot timeseries data
 
-function createsvg(selector, data, x_domain, y_domain) {
+function createsvg(selector, x_domain, y_domain) {
+  console.log("created svg")
   const margin = { top: 10, right: 30, bottom: 30, left: 60 },
     width = 380 - margin.left - margin.right,
     height = 150 - margin.top - margin.bottom;
@@ -19,9 +91,10 @@ function createsvg(selector, data, x_domain, y_domain) {
   const x = d3.scaleLinear()
     .domain(x_domain)
     .range([0, width]);
-  svg.append("g")
+  const x_axis = svg.append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+    .call(d3.axisBottom(x))
+    .attr("class", "x_axis");
 
   // Add X axis label
   svg.append("text")
@@ -32,11 +105,12 @@ function createsvg(selector, data, x_domain, y_domain) {
     .text("years");
 
   // Add Y axis
-  const y = d3.scaleLinear()
-    .domain(y_domain)
-    .range([height, 0]);
+
+  const y = d3.scaleLinear().range([height, 0]).domain(y_domain);
+  const y_axis = d3.axisLeft().scale(y);
   svg.append("g")
-    .call(d3.axisLeft(y));
+    .attr("class", "myYaxis")
+    .call(y_axis)
 
   // Add Y axis label
   svg.append("text")
@@ -46,7 +120,7 @@ function createsvg(selector, data, x_domain, y_domain) {
     .attr("font-weight", "normal")
     .attr("transform", "rotate(-90)")
     .text("% of the population infected");
-  return [svg, x, y];
+  return [svg, x, x_axis, y, y_axis];
 }
 
 function plotTimeseries(svg, x, y, data, color, opacity = 1) {
@@ -120,33 +194,33 @@ async function loadAndPlotScrollableTimeseries(dataFile, selector, color, t1 = n
 }
 
 
-// Load data from CSV file and plot a timeseries
-async function loadAndPlotTimeseries(dataFile, selector, color, x_domain = "auto", y_domain = "auto") {
-  // Load data from CSV file
-  try {
-    const data = await d3.csv(dataFile);
-    // Convert t and I to numbers
-    data.forEach(function (d) {
-      d.t = +d.t;
-      d.I = +d.I;
-    });
+// // Load data from CSV file and plot a timeseries
+// async function loadAndPlotTimeseries(dataFile, selector, color, x_domain = "auto", y_domain = "auto") {
+//   // Load data from CSV file
+//   try {
+//     const data = await d3.csv(dataFile);
+//     // Convert t and I to numbers
+//     data.forEach(function (d) {
+//       d.t = +d.t;
+//       d.I = +d.I;
+//     });
 
-    if (x_domain == "auto") {
-      x_domain = d3.extent(data, d => d.t)
-    }
-    if (y_domain == "auto") {
-      y_domain = d3.extent(data, d => d.I)
-    }
+//     if (x_domain == "auto") {
+//       x_domain = d3.extent(data, d => d.t)
+//     }
+//     if (y_domain == "auto") {
+//       y_domain = d3.extent(data, d => d.I)
+//     }
 
-    // Plot
-    const [svg, x, y] = createsvg(selector, data, x_domain, y_domain);
-    plotTimeseries(svg, x, y, data, color);
-    return { svg, x, y };
-  } catch (error) {
-    console.error(error);
-    throw error; // Re-throw the error
-  }
-}
+//     // Plot
+//     const [svg, x, y] = createsvg(selector, x_domain, y_domain);
+//     plotTimeseries(svg, x, y, data, color);
+//     return { svg, x, y };
+//   } catch (error) {
+//     console.error(error);
+//     throw error; // Re-throw the error
+//   }
+// }
 
 // function addToPlot(svg, x, y, dataFile, color) {
 //   console.log("test")
